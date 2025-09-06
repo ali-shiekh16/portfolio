0d003593bf0fe58dc0ebbbc8bb41a5a642b2c165
Ali <ali.shiekh16@outlook.com>
2025-09-06 19:00:29 +0500

    Add projects section

--- src/app/(app)/(root)/page.tsx
+++ src/app/(app)/(root)/page.tsx
@@ -11,7 +11,7 @@ import { Experiences } from "@/features/profile/components/experiences";
 import { Overview } from "@/features/profile/components/overview";
 import { ProfileCover } from "@/features/profile/components/profile-cover";
 import { ProfileHeader } from "@/features/profile/components/profile-header";
-// import { Projects } from "@/features/profile/components/projects";
+import { Projects } from "@/features/profile/components/projects";
 import { SocialLinks } from "@/features/profile/components/social-links";
 import { TeckStack } from "@/features/profile/components/teck-stack";
 import { USER } from "@/features/profile/data/user";
@@ -45,6 +45,9 @@ export default function Page() {
         <Separator />
 
         <Experiences />
+        <Separator />
+
+        <Projects />
         <Separator />
 
         <Education />
